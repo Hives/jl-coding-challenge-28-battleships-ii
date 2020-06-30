086fd4c0589ef2a74ab2096e5f038d4cec7efad8
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-06-30 20:54:42 +0100

    Pull configuration out of main function
    See https://ktor.io/quickstart/quickstart/gradle.html#intellij-improve-the-app-with-the-application-object
    
    Could only get this to work using Java 8 :-/

--- src/main/kotlin/battleships/App.kt
+++ src/main/kotlin/battleships/App.kt
@@ -3,22 +3,19 @@ package battleships
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType.Text.Html
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
 import kotlin.math.floor
 
-fun Application.module() {
+fun Application.main() {
+    install(DefaultHeaders)
     install(Routing) {
         get("/") {
             call.respondText("Hello mum ${floor(100 * Math.random()).toInt()}", Html)
         }
     }
-}
-
-fun main() {
-    embeddedServer(Netty, 8080, watchPaths = listOf("AppKt"), module = Application::module).start()
 }
